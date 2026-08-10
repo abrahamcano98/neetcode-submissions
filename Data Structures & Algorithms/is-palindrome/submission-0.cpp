61c66c6da6be2d30aedb79b651383b4061c1b448
abrahamcano98 <51884516+abrahamcano98@users.noreply.github.com>
2026-08-10 17:55:35 +0200

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int j = n - 1;
+        int i = 0;
+
+        while (i < j)
+        {
+            while (i < j && !alphanum(s[i]))
+            {
+                i++;
+            }
+
+            while (i < j && !alphanum(s[j]))
+            {
+                j--;
+            }
+
+            if (tolower(s[i]) != tolower(s[j]))
+            {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    bool alphanum(char c)
+    {
+        if ((c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            (c >= '0' && c <= '9'))
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
